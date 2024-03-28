99e3b1c12853d835efe37fba7f5b98876e20b7cd
raphaelanjos1 <raphael-anjos@live.com>
2024-03-28 00:47:34 -0300

    config vim-options and add lualine plugin

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -19,8 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
-
-local opts = {}
 
 require("lazy").setup(plugins, opts)
 vim.keymap.set('n', '<C-f>', 'gg=G', {noremap = true, silent = true})
